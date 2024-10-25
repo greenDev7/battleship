66e42dd00035361de8921af2ff124edf517bb2ba
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2024-10-25 16:36:08 +0300

    Добавлен компонент для хранения кораблей перед расстановкой

---
+++ src/components/FleetComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <BattleGridComponent :x-size="5" :y-size="7" />
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BattleGridComponent from "../components/BattleGridComponent.vue";
+
+export default defineComponent({
+    name: "FleetComponent",
+
+    components: { BattleGridComponent }
+
+})
+</script>
+
+
+<style lang="css" scoped>
+/* table {
+    border: 1px solid blue;
+} */
+</style>
