2c87cabb8d976f746c4dd0ca87b9da795ea9c255
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2024-11-13 14:52:23 +0300

    Убрал ползунок с масштабированием и соответствующие автосвойства

--- src/views/BattleShipView.vue
+++ src/views/BattleShipView.vue
@@ -1,8 +1,4 @@
 <template>
-    <div class="mainTableRow2">
-        <p><label for="scale">Масштаб</label></p>
-        <input type="range" id="scale" min="0.7" max="1.3" step="0.1" v-model="scaleParameter2">
-    </div>
     <BattleBoardComponent class="inline" />
 </template>
 
@@ -14,22 +10,7 @@ import { defineComponent } from 'vue'
 export default defineComponent({
     name: "BattleShipView",
 
-    components: { BattleBoardComponent },
-
-    computed: {
-        scaleParameter2: {
-
-            get() {
-                return GameStore.state.scaleParameter;
-            },
-
-            set(value: number) {
-                GameStore.commit("updateScaleParameter", value);
-            }
-
-        }
-    }
-
+    components: { BattleBoardComponent }
 })
 </script>
 
