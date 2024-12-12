6c6483ccf2d595faceba7caaf5e29875c3668089
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2024-12-12 15:01:16 +0300

    Обновлены интерфейсы для передачи данных между клиентами

--- src/model/WSDataTransferRoot.ts
+++ src/model/WSDataTransferRoot.ts
@@ -1,20 +1,27 @@
-export default interface WSDataTransferRoot {
+interface MsgType {
     msg_type: string;
-    data: TransferLevel2;
-    is_status_ok: boolean;
 };
-
-
-interface TransferLevel2 {
-    enemy_nickname: string;
-    turn_to_shoot: boolean;
-    enemy_client_id: string;
-    shot_location: Loc;
-    shot_result: number;
-};
-
 
 interface Loc {
     _x: number;
     _y: number;
-}
+};
+
+interface FireResponse extends MsgType {
+    shot_result: number;
+    enemy_client_id: string;
+    edgeLocs: Array<Loc>;
+};
+
+interface TransferLevel2 extends FireResponse {
+    enemy_nickname: string;
+    turn_to_shoot: boolean;
+    shot_location: Loc;
+};
+
+interface WSDataTransferRoot extends MsgType {
+    data: TransferLevel2;
+    is_status_ok: boolean;
+};
+
+export type { FireResponse as FireResponseType, WSDataTransferRoot as WSDataTransferRootType, Loc as LocType }
