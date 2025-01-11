4fc2def0eaa8791c5126e5ba395631ba1571c175
andrey.zelenyy <andrey.zelenyy@south.rt.ru>
2025-01-11 10:00:50 +0300

    Добавлен компонент для вывода информации об игре

---
+++ src/components/StatisticsInfoComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>6</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "StatisticsInfoComponent",
+
+  props: {
+    rowCount: { type: Number, default: 10 },
+    dataRows: { type: Object, default: null },
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+});
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container text-center">
+    <h1>Привет, игрок!</h1>
+    <br />
+    <p>
+      Любые замечания, вопросы и предложения по игре можешь направлять на
+      <a href="https://simplegames2025@gmail.com">simplegames2025@gmail.com</a>
+    </p>
+
+    <StatisticsInfoComponent />
   </div>
 </template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import StatisticsInfoComponent from "@/components/StatisticsInfoComponent.vue";
+
+export default defineComponent({
+  components: {
+    StatisticsInfoComponent,
+  },
+
+  data() {
+    return {
+      
+    };
+  },
+  async mounted() {},
+});
+</script>
